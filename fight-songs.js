cf2cf85aa3b739f20de6de83a02a33de35346478
Brandon <brandoncrichie@gmail.com>
2018-12-11 14:31:33 -0800

    Basic Node App Assignment Complete

---
+++ fight-songs.js
@@ -0,0 +1,7 @@
+exports.eaglesFightSong = function(){
+    console.log("Fly, Eagles Fly On the road to victory Fight! Fight! Fight! Fight, Eagles fight Score a touchdown 1, 2, 3! (1! 2! 3!) Hit 'em low! Hit 'em high! And watch our Eagles fly! Fly, Eagles Fly! On the road to victory! E-A-G-L-E-S! Eagles!");
+}
+
+exports.bearsFightSong = function (){
+    console.log("Bear down, Chicago Bears, make every play clear the way to victory; Bear down, Chicago Bears, put up a fight with a might so fearlessly. We'll never forget the way you thrilled the nation with your T-formation. Bear down, Chicago Bears, and let them know why you're wearing the crown. You're the pride and joy of Illinois, Chicago Bears, bear down.");
+}
---
+++ main.js
@@ -0,0 +1,18 @@
+const greetings = require("./greeting.js");
+const fightSongs = require("./fight-songs.js");
+
+//"Hello"
+greetings.greetingEnglish();
+
+//"Konichiwa"
+greetings.greetingJapanese();
+
+//"Hola"
+greetings.greetingSpanish();
+
+//"Hallo"
+greetings.greetingIcelandic();
+
+fightSongs.eaglesFightSong();
+
+fightSongs.bearsFightSong();
